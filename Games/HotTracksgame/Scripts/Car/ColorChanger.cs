1d27c9ee623778e71130ad9234d166c80ab64812
Alberto Alvarado Jr <65637580+albertgs09@users.noreply.github.com>
2023-05-19 21:04:14 -0500

    Update ColorChanger.cs

--- Games/HotTracksgame/Scripts/Car/ColorChanger.cs
+++ Games/HotTracksgame/Scripts/Car/ColorChanger.cs
@@ -6,15 +6,13 @@ public class ColorChanger : MonoBehaviour
 {
     private MeshFilter bodyRef;
     public Mesh[] color;
-    int i = 0;
-    // Start is called before the first frame update
-    void Start()
+    private int i = 0;
+    
+    private void Start()
     {
         bodyRef = GetComponent<MeshFilter>();
         i = PlayerPrefs.GetInt("CarChoice", 0);
         bodyRef.mesh = color[i];
 
-    }
-
-    
+    }    
 }
