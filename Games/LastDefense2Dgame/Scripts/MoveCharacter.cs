ea0319a16ba9f85f5f5c0b9b5385c72d06f342b8
Alberto Alvarado Jr <65637580+albertgs09@users.noreply.github.com>
2023-05-19 21:36:07 -0500

    Update MoveCharacter.cs

--- Games/LastDefense2Dgame/Scripts/MoveCharacter.cs
+++ Games/LastDefense2Dgame/Scripts/MoveCharacter.cs
@@ -9,24 +9,16 @@ public class MoveCharacter : MonoBehaviour
     private float x_dir;
     private float horizontal;
     private float moveSpeed = 5.0f;
-    // Start is called before the first frame update
-    void Start()
+   
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
-    {
-        
+    private void FixedUpdate()
+    { 
         //mobile input
         x_dir = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
         rb.velocity = new Vector2(x_dir, 0f);
-        
-
-        /*
-        //pc input
-        horizontal = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(horizontal, 0f) * moveSpeed;
-        */
     }
 }
