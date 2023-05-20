5d1f444542eaf1d617080430bc9289137e277186
Alberto Alvarado Jr <65637580+albertgs09@users.noreply.github.com>
2023-05-19 21:25:34 -0500

    Update Destroy.cs

--- Games/LastDefense2Dgame/Scripts/Destroy.cs
+++ Games/LastDefense2Dgame/Scripts/Destroy.cs
@@ -7,8 +7,6 @@ public class Destroy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
-        {
             Destroy(collision.gameObject);
-        }
     }
 }
