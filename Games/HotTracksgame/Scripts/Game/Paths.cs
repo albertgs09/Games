8814a0aa466f06cf9ff698ddac5a8f2ad23e86e4
Alberto Alvarado Jr <65637580+albertgs09@users.noreply.github.com>
2023-05-19 21:09:07 -0500

    Update Paths.cs

--- Games/HotTracksgame/Scripts/Game/Paths.cs
+++ Games/HotTracksgame/Scripts/Game/Paths.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Destroys path block ways when player collides with triggers
+//then activates the next one
 public class Paths : MonoBehaviour
 {
     public GameObject blockadePrev, blockadeNext;
