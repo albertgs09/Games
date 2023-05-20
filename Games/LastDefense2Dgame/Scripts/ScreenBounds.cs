a413f70c88ca5665fef45a065b86c385e8e48b6b
Alberto Alvarado Jr <65637580+albertgs09@users.noreply.github.com>
2023-05-19 21:37:59 -0500

    Update ScreenBounds.cs

--- Games/LastDefense2Dgame/Scripts/ScreenBounds.cs
+++ Games/LastDefense2Dgame/Scripts/ScreenBounds.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class ScreenBounds : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.4f, 2.4f)
