4715d60807a19633620cd67303b86c9992b670d1
Alberto Alvarado Jr <65637580+albertgs09@users.noreply.github.com>
2023-05-19 21:50:50 -0500

    Update SceneController.cs

--- Games/Trials/Scripts/Scares/SceneController.cs
+++ Games/Trials/Scripts/Scares/SceneController.cs
@@ -7,15 +7,15 @@ public class SceneController : MonoBehaviour
 {
     public float time;
     public string scene;
-    // Start is called before the first frame update
-    void Start()
+    
+    private void Start()
     {
         StartCoroutine(StartTime(time));
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
 
-   IEnumerator StartTime(float time)
+   private IEnumerator StartTime(float time)
     {
         yield return new WaitForSeconds(time);
         SceneManager.LoadScene(scene);
