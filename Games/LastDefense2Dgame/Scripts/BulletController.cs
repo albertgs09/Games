4220f76f5498166502738abcd00e03e19db836f5
Alberto Alvarado Jr <65637580+albertgs09@users.noreply.github.com>
2023-05-19 21:25:07 -0500

    Update BulletController.cs

--- Games/LastDefense2Dgame/Scripts/BulletController.cs
+++ Games/LastDefense2Dgame/Scripts/BulletController.cs
@@ -6,20 +6,18 @@ public class BulletController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public int speed = 100;
-
     public GameObject explosion;
-    // Start is called before the first frame update
-    void Start()
+    
+   private void Start()
     {
+    //Moves bullet up
         rb.velocity = Vector2.up * speed;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Enemy")
-        {
-            Destroy(gameObject);
-        }
+            Destroy(gameObject);  
     }
   
 }
