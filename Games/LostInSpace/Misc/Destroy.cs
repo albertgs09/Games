a5851d9105ce33ad1bc5d49e7c5732a0b2a7f528
Alberto Alvarado Jr <65637580+albertgs09@users.noreply.github.com>
2023-09-25 17:45:45 -0500

    Update Destroy.cs

--- Games/LostInSpace/Misc/Destroy.cs
+++ Games/LostInSpace/Misc/Destroy.cs
@@ -4,9 +4,5 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
-
-    private void Start()
-    {
-        Destroy(gameObject, 3) ;
-    }
+    private void Start() =>  Destroy(gameObject, 3) ;
 }
